299669e6ef34b26d585e43f6334a464a3cef65db
remyapeevee <remyapv118@gmail.com>
2026-01-31 09:29:55 +0530

    Add files via upload

---
+++ JDBCDemo.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+public class JDBCDemo {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/testdb";
+        String username = "root";
+        String password = "root";
+
+        try {
+            // Load MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connect to the database
+            Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connected to database.");
+
+            // Execute a query
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id, name FROM users");
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id") + ", Name: " + rs.getString("name"));
+            }
+
+            // Clean up
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
